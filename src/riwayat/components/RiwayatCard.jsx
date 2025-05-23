19d8fcbf3a71c1a1ee5cbc8a442fd8b900dc8f9f
Athar Adista <athar0165@gmail.com>
2025-05-23 23:13:45 +0700

    feat: finish generate 1

--- src/riwayat/components/RiwayatCard.jsx
+++ src/riwayat/components/RiwayatCard.jsx
@@ -3,105 +3,127 @@
 	https://amanah.cs.ui.ac.id/research/ifml-regen
 	version 3.9.0
 */
-import React from 'react'
-import { Link } from "react-router";
-import { useParams } from "@/commons/hooks/useParams"
+import React from "react";
+import environment from "@/commons/utils/environment";
+import tokenManager from "@/commons/utils/token";
 
-import { useAuth } from '@/commons/auth'
-import { Button } from '@/commons/components';
+import { Link } from "react-router";
+import { useParams } from "@/commons/hooks/useParams";
+
+import { useAuth } from "@/commons/auth";
+import { Button } from "@/commons/components";
 
 import * as Layouts from "@/commons/layouts";
 
-const RiwayatCard = ({ listRiwayat,
-		 
-
-	}) => {
+const RiwayatCard = ({ listRiwayat, refresh }) => {
   const { checkPermission } = useAuth();
-  const {  } = useParams()
-  
+  const {} = useParams();
+
   return (
     <Layouts.ListComponentCardLayout
       items={[listRiwayat]}
-  	
-  	itemsAttrs={[
-          {
-            id: "idPemesanan",
-            condition: "",
-            label: "Id Pemesanan",
-  		  featureName: "idPemesanan",
-            editable: false
-          }
-  ,
-          {
-            id: "startDate",
-            condition: "",
-            label: "Start Date",
-  		  featureName: "startDate",
-            editable: false
-          }
-  ,
-          {
-            id: "endDate",
-            condition: "",
-            label: "End Date",
-  		  featureName: "endDate",
-            editable: false
-          }
-  ,
-          {
-            id: "totalPay",
-            condition: "",
-            label: "Total Harga",
-  		  featureName: "totalPay",
-            editable: false
-          }
-  ,
-          {
-            id: "statusPemesanan",
-            condition: "",
-            label: "Status Pemesanan",
-  		  featureName: "statusPemesanan",
-            editable: false
-          }
-  ,
-          {
-            id: "detail",
-            condition: "",
-            label: "Detail",
-  		  featureName: "detail",
-            editable: false
-          }
-  ,
-          {
-            id: "createdAt",
-            condition: "",
-            label: "Created At",
-  		  featureName: "createdAt",
-            editable: false
-          }
-  ]}
-      itemsEvents={(riwayatItem) =>{
-        
-        if (riwayatItem.statusPemesanan === "Success"){
+      itemsAttrs={[
+        {
+          id: "idPemesanan",
+          condition: "",
+          label: "Id Pemesanan",
+          featureName: "idPemesanan",
+          editable: false,
+        },
+        {
+          id: "startDate",
+          condition: "",
+          label: "Start Date",
+          featureName: "startDate",
+          editable: false,
+        },
+        {
+          id: "endDate",
+          condition: "",
+          label: "End Date",
+          featureName: "endDate",
+          editable: false,
+        },
+        {
+          id: "totalPay",
+          condition: "",
+          label: "Total Harga",
+          featureName: "totalPay",
+          editable: false,
+        },
+        {
+          id: "statusPemesanan",
+          condition: "",
+          label: "Status Pemesanan",
+          featureName: "statusPemesanan",
+          editable: false,
+        },
+        {
+          id: "detail",
+          condition: "",
+          label: "Detail",
+          featureName: "detail",
+          editable: false,
+        },
+        {
+          id: "createdAt",
+          condition: "",
+          label: "Created At",
+          featureName: "createdAt",
+          editable: false,
+        },
+      ]}
+      itemsEvents={(riwayatItem) => {
+        if (riwayatItem.statusPemesanan === "Success") {
+          return [
+            <Button
+              key="selesaikan"
+              size="sm"
+              variant="success"
+              onClick={async () => {
+                try {
+                  const { getToken } = tokenManager();
+	                const token = getToken();
+                  const res = await fetch(
+                    `${environment.rootApi}/call/pemesanan/update-kamar?idPemesanan=${riwayatItem.idPemesanan}`,
+                    {
+                      method: "PUT", // atau "POST" sesuai backend kamu
+                      params: {token},
+                      headers: {
+                        "Content-Type": "application/json",
+                      },
+                      body: JSON.stringify({ status: "Selesai" }), // atau payload lain sesuai kebutuhan
+                    }
+                  );
+
+                  if (!res.ok) throw new Error("Gagal mengupdate status");
+                  refresh();
+                  
+                } catch (err) {
+                  alert("Gagal menyelesaikan pemesanan.");
+                  console.error(err);
+                }
+              }}
+            >
+              Selesaikan Pemesanan
+            </Button>,
+          ];
+        }
+
+        if (riwayatItem.statusPemesanan == "Completed"){
           return null;
         }
 
         return [
-        
-        <Link to={`/payment-kredit/${riwayatItem.idPemesanan}`}>
-          <Button
-            size="sm"
-            variant=
-                "primary"
-          >
-            Pay
-          </Button>
-        </Link>
-        
-        
-  	]}}
+          <Link to={`/payment-kredit/${riwayatItem.idPemesanan}`}>
+            <Button size="sm" variant="primary">
+              Pay
+            </Button>
+          </Link>,
+        ];
+      }}
     />
-  )	
+  );
 };
 
 export default RiwayatCard;
--- src/riwayat/containers/RiwayatPage.jsx
+++ src/riwayat/containers/RiwayatPage.jsx
@@ -3,72 +3,61 @@
 	https://amanah.cs.ui.ac.id/research/ifml-regen
 	version 3.9.0
 */
-import React, { useEffect, useState, useContext} from 'react'
-import { Button, Spinner } from "@/commons/components"
-import * as Layouts from '@/commons/layouts';
+import React, { useEffect, useState, useContext } from "react";
+import { Button, Spinner } from "@/commons/components";
+import * as Layouts from "@/commons/layouts";
 import { Link } from "react-router";
-import { useParams } from "@/commons/hooks/useParams"
-import { HeaderContext } from "@/commons/components"
+import { useParams } from "@/commons/hooks/useParams";
+import { HeaderContext } from "@/commons/components";
 import { useNavigate } from "react-router";
-import { useAuth } from '@/commons/auth';
+import { useAuth } from "@/commons/auth";
 import RiwayatCard from "../components/RiwayatCard";
 
-import getListRiwayat from '../services/getListRiwayat'
-const RiwayatPage = props => {
-const { checkPermission } = useAuth();
+import getListRiwayat from "../services/getListRiwayat";
+const RiwayatPage = (props) => {
+  const { checkPermission } = useAuth();
 
-	const [isLoading, setIsLoading] = useState({
-	listRiwayat: false,
+  const [isLoading, setIsLoading] = useState({
+    listRiwayat: false,
+  });
+  const { setTitle } = useContext(HeaderContext);
 
-	});
-	const { setTitle } = useContext(HeaderContext);
+  const [listRiwayat, setListRiwayat] = useState();
+  const fetchData = async () => {
+    try {
+      setIsLoading((prev) => ({ ...prev, listRiwayat: true }));
+      const { data: listRiwayat } = await getListRiwayat({});
 
-const [listRiwayat, setListRiwayat] = useState()
-	
-	
-	
-	useEffect(() => {
-		
+      setListRiwayat(listRiwayat.data);
+    } finally {
+      setIsLoading((prev) => ({ ...prev, listRiwayat: false }));
+    }
+  };
 
-		const fetchData = async () => {
-			try {
-				setIsLoading(prev => ({...prev, listRiwayat: true}))
-				const { data: listRiwayat } = await getListRiwayat({  })
+  useEffect(() => {
+    fetchData();
+  }, []);
 
-				setListRiwayat(listRiwayat.data)
-			} finally {
-				setIsLoading(prev => ({...prev, listRiwayat: false}))
-			}
-		}
-		fetchData()
-  	}, [])
-
-	
-	useEffect(() => {
-		setTitle("Riwayat Page")
-	}, []);
-return (
-	<Layouts.ViewContainerLayout
-		buttons={
-			<>
-			<></>
-			</>
-		}
-	>
-<Layouts.ListContainerCardLayout
-	title={"List Riwayat"}
-	singularName={"Riwayat"}
-	items={[listRiwayat]}
-	isLoading={isLoading.listRiwayat}
->
-	<RiwayatCard
-		listRiwayat={listRiwayat}
-		
-  	/>
-</Layouts.ListContainerCardLayout>
-
-	</Layouts.ViewContainerLayout>
-  )
-}
-export default RiwayatPage
-
+  useEffect(() => {
+    setTitle("Riwayat Page");
+  }, []);
+  return (
+    <Layouts.ViewContainerLayout
+      buttons={
+        <>
+          <></>
+        </>
+      }
+    >
+      <Layouts.ListContainerCardLayout
+        title={"List Riwayat"}
+        singularName={"Riwayat"}
+        items={[listRiwayat]}
+        isLoading={isLoading.listRiwayat}
+      >
+        <RiwayatCard listRiwayat={listRiwayat} refresh={fetchData}/>
+      </Layouts.ListContainerCardLayout>
+    </Layouts.ViewContainerLayout>
+  );
+};
+export default RiwayatPage;
